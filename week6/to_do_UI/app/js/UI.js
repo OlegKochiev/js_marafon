abf18df811770ae0cfa4021c4298376b261a4b47
Kochiev Oleg <developer-artisan@mail.ru>
2022-02-23 13:39:27 +0300

    was changed import-export files

--- week6/to_do_UI/app/js/UI.js
+++ week6/to_do_UI/app/js/UI.js
@@ -1,30 +1,4 @@
-const STATUS = {
-  DONE: "Done",
-  TO_DO: "To do"
-}
-const PRIORITY = {
-  HIGH: "high",
-  LOW: "low"
-}
-
-const list = [
-  // {
-  //   name: "Genry",
-  //   priority: "LOW",
-  //   status: "Done"
-  // },
-  // {
-  //   name: "Harry",
-  //   priority: "LOW",
-  //   status: "Done"
-  // },
-  // {
-  //   name: "Ronald",
-  //   priority: "HIGH",
-  //   status: "To do"
-  // }
-]
-
+import { STATUS, PRIORITY, list } from './main.js';
 
 const inputHigh = document.getElementById('inputHigh');
 const inputLow = document.getElementById('inputLow');
@@ -37,77 +11,73 @@ inputLow.addEventListener('keydown', listenerForInput);
 btnHigh.addEventListener('click', addTask);
 btnLow.addEventListener('click', addTask);
 
-
-
 function addTask(event) {
-  const task = getNewTask(event);
-  const taskNode = getNewNode(task);
-  const toDoList = getToDOList(task);
+    const task = getNewTask(event);
+    const taskNode = getNewNode(task);
+    const toDoList = getToDOList(task);
 
-  toDoList.prepend(taskNode);
+    toDoList.prepend(taskNode);
 }
 
 function getNewTask(event) {
-  const parent = event.target.parentNode;
-  const name = parent.querySelector('input').value;
-  parent.querySelector('input').value = '';
-  const status = "To do";
-  const priority = parent.id === 'highPriority' ? 'HIGH' : 'LOW';
-  return {
-    name,
-    status,
-    priority
-  };
+    const parent = event.target.parentNode;
+    const name = parent.querySelector('input').value;
+    parent.querySelector('input').value = '';
+    const status = STATUS.TO_DO;
+    const priority = parent.id === 'highPriority' ? PRIORITY.HIGH : PRIORITY.LOW;
+    return {
+        name,
+        status,
+        priority
+    };
 }
 
 function getNewNode(task) {
-  let li = document.createElement('li');
-  let checkbox = document.createElement('input');
-  let p = document.createElement('p');
-  let btnDel = document.createElement('button');
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    let p = document.createElement('p');
+    let btnDel = document.createElement('button');
 
-  li.classList.add('to-do__item');
+    li.classList.add('to-do__item');
 
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('to-do__task-checkbox');
-  checkbox.addEventListener('click', changeTaskStatus, task);
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('to-do__task-checkbox');
+    checkbox.addEventListener('click', changeTaskStatus, task);
 
-  p.textContent = task.name;
-  p.classList.add('to-do__task-desc');
+    p.textContent = task.name;
+    p.classList.add('to-do__task-desc');
 
-  btnDel.type = 'button'
-  btnDel.classList.add('to-do__del-btn');
-  btnDel.addEventListener('click', delTask);
+    btnDel.type = 'button'
+    btnDel.classList.add('to-do__del-btn');
+    btnDel.addEventListener('click', delTask);
 
-  li.appendChild(checkbox);
-  li.appendChild(p);
-  li.appendChild(btnDel);
+    li.appendChild(checkbox);
+    li.appendChild(p);
+    li.appendChild(btnDel);
 
-  return li;
+    return li;
 }
 
 function getToDOList(task) {
-  let toDoList;
-  if (task.priority === 'HIGH') {
-    toDoList = document.getElementById('highPriorityList');
-  } else {
-    toDoList = document.getElementById('lowPriorityList');
-  }
-  return toDoList;
+    let toDoList;
+    if (task.priority === PRIORITY.HIGH) {
+        toDoList = document.getElementById('highPriorityList');
+    } else {
+        toDoList = document.getElementById('lowPriorityList');
+    }
+    return toDoList;
 }
-
 
 function delTask(event) {
-  let toDoNode = event.target.parentNode;
-  toDoNode.remove();
+    let toDoNode = event.target.parentNode;
+    toDoNode.remove();
 }
 
-
 function listenerForInput(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    addTask(event);
-  }
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addTask(event);
+    }
 }
 
 function changeTaskStatus(task) {
