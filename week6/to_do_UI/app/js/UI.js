b8a340dcc76075eb7d1f37098a0240e4eb100886
Kochiev Oleg <developer-artisan@mail.ru>
2022-02-25 12:52:18 +0300

    was swap places import and export functions in ui.js and main.js

--- week6/to_do_UI/app/js/UI.js
+++ week6/to_do_UI/app/js/UI.js
@@ -1,73 +1,91 @@
-function addTask(event, STATUS, PRIORITY) {
-    const task = getNewTask(event, STATUS, PRIORITY);
-    if (isNotEmpty(task)) {
-        const taskNode = getNewNode(task);
-        const toDoList = getToDOList(task, PRIORITY);
-        toDoList.prepend(taskNode);
-    }
+import {
+	STATUS,
+	PRIORITY
+} from './main.js';
+
+const inputHigh = document.getElementById('inputHigh');
+const inputLow = document.getElementById('inputLow');
+inputHigh.addEventListener('keydown', (event) => listenerForInput(event));
+inputLow.addEventListener('keydown', (event) => listenerForInput(event));
+
+
+const btnHigh = document.getElementById('btnHigh');
+const btnLow = document.getElementById('btnLow');
+
+
+btnHigh.addEventListener('click', (event) => addTask(event));
+btnLow.addEventListener('click', (event) => addTask(event));
+
+function addTask(event) {
+	const task = getNewTask(event);
+	if (isNotEmpty(task)) {
+		const taskNode = getNewNode(task);
+		const toDoList = getToDOList(task);
+		toDoList.prepend(taskNode);
+	}
 }
 
-function getNewTask(event, STATUS, PRIORITY) {
-    const parent = event.target.parentNode;
-    const name = parent.querySelector('input').value;
-    parent.querySelector('input').value = '';
-    const status = STATUS.TO_DO;
-    const priority = parent.id === 'highPriority' ? PRIORITY.HIGH : PRIORITY.LOW;
-    return {
-        name,
-        status,
-        priority
-    };
+function getNewTask(event) {
+	const parent = event.target.parentNode;
+	const name = parent.querySelector('input').value;
+	parent.querySelector('input').value = '';
+	const status = STATUS.TO_DO;
+	const priority = parent.id === 'highPriority' ? PRIORITY.HIGH : PRIORITY.LOW;
+	return {
+		name,
+		status,
+		priority
+	};
 }
 
 function getNewNode(task) {
-    let li = document.createElement('li');
-    let label = document.createElement('label');
-    let checkbox = document.createElement('input');
-    let span = document.createElement('span');
-    let p = document.createElement('p');
-    let btnDel = document.createElement('button');
+	let li = document.createElement('li');
+	let label = document.createElement('label');
+	let checkbox = document.createElement('input');
+	let span = document.createElement('span');
+	let p = document.createElement('p');
+	let btnDel = document.createElement('button');
 
-    li.classList.add('to-do__item');
-    label.classList.add('to-do__checkbox-wrapper');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('to-do__task-checkbox');
-    checkbox.addEventListener('click', changeTaskStatus, task);
-    p.textContent = task.name;
-    p.classList.add('to-do__task-desc');
-    btnDel.type = 'button'
-    btnDel.classList.add('to-do__del-btn');
-    btnDel.addEventListener('click', delTask);
+	li.classList.add('to-do__item');
+	label.classList.add('to-do__checkbox-wrapper');
+	checkbox.type = 'checkbox';
+	checkbox.classList.add('to-do__task-checkbox');
+	checkbox.addEventListener('click', changeTaskStatus, task);
+	p.textContent = task.name;
+	p.classList.add('to-do__task-desc');
+	btnDel.type = 'button'
+	btnDel.classList.add('to-do__del-btn');
+	btnDel.addEventListener('click', delTask);
 
-    label.appendChild(checkbox);
-    label.appendChild(span);
-    li.appendChild(label);
-    li.appendChild(p);
-    li.appendChild(btnDel);
+	label.appendChild(checkbox);
+	label.appendChild(span);
+	li.appendChild(label);
+	li.appendChild(p);
+	li.appendChild(btnDel);
 
-    return li;
+	return li;
 }
 
-function getToDOList(task, PRIORITY) {
-    let toDoList;
-    if (task.priority === PRIORITY.HIGH) {
-        toDoList = document.getElementById('highPriorityList');
-    } else {
-        toDoList = document.getElementById('lowPriorityList');
-    }
-    return toDoList;
+function getToDOList(task) {
+	let toDoList;
+	if (task.priority === PRIORITY.HIGH) {
+		toDoList = document.getElementById('highPriorityList');
+	} else {
+		toDoList = document.getElementById('lowPriorityList');
+	}
+	return toDoList;
 }
 
 function delTask(event) {
-    let toDoNode = event.target.parentNode;
-    toDoNode.remove();
+	let toDoNode = event.target.parentNode;
+	toDoNode.remove();
 }
 
-function listenerForInput(event, STATUS, PRIORITY) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        addTask(event, STATUS, PRIORITY);
-    }
+function listenerForInput(event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		addTask(event);
+	}
 }
 
 function changeTaskStatus(task) {
@@ -75,19 +93,19 @@ function changeTaskStatus(task) {
 }
 
 function isNotEmpty(task) {
-    if (task.name !== '') {
-        return true;
-    } else {
-        return false;
-    }
+	if (task.name !== '') {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 export {
-    addTask,
-    getNewTask,
-    getNewNode,
-    getToDOList,
-    delTask,
-    listenerForInput,
-    changeTaskStatus
+	addTask,
+	getNewTask,
+	getNewNode,
+	getToDOList,
+	delTask,
+	listenerForInput,
+	changeTaskStatus
 }
--- week6/to_do_UI/app/js/main.js
+++ week6/to_do_UI/app/js/main.js
@@ -1,10 +1,3 @@
-import {
-  addTask,
-  delTask,
-  listenerForInput,
-  changeTaskStatus
-} from './UI.js'
-
 const STATUS = {
   DONE: "Done",
   TO_DO: "To do"
@@ -16,15 +9,7 @@ const PRIORITY = {
 
 const list = []
 
-const inputHigh = document.getElementById('inputHigh');
-const inputLow = document.getElementById('inputLow');
-inputHigh.addEventListener('keydown', (event) => listenerForInput(event, STATUS, PRIORITY));
-inputLow.addEventListener('keydown', (event) => listenerForInput(event, STATUS, PRIORITY));
-
-
-const btnHigh = document.getElementById('btnHigh');
-const btnLow = document.getElementById('btnLow');
-
-
-btnHigh.addEventListener('click', (event) => addTask(event, STATUS, PRIORITY));
-btnLow.addEventListener('click', (event) => addTask(event, STATUS, PRIORITY));
+export {
+  STATUS,
+  PRIORITY
+}
