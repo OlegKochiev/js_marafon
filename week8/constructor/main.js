b3bd67c3dcf0ceb483b45ca397cd578af254562c
Kochiev Oleg <developer-artisan@mail.ru>
2022-04-02 12:02:11 +0300

    was learned rest parameters and  extension operator js

---
+++ week8/constructor/main.js
@@ -0,0 +1,18 @@
+// Остаточные параметры 
+function sumAll(...args) { // args — имя массива
+  let sum = 0;
+
+  for (let arg of args) sum += arg;
+
+  return sum;
+}
+
+alert(sumAll(1)); // 1
+alert(sumAll(1, 2)); // 3
+alert(sumAll(1, 2, 3)); // 6
+
+
+// Оператор расширения
+let arr = [3, 5, 1];
+
+alert(Math.max(...arr)); // 5 (оператор "раскрывает" массив в список аргументов)
